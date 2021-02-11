ef0cd034206392aac54d255f848ff45d60432395
CHANDU <dchsrao@gmail.com>
2021-02-11 14:23:44 +0530

    Add files via upload

---
+++ spring-boot-stripe-payment-integration-master/src/main/java/com/stripe/controllers/PaymentController.java
@@ -0,0 +1,56 @@
+package com.stripe.controllers;
+
+import com.stripe.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.stripe.model.Coupon;
+import com.stripe.service.StripeService;
+import com.stripe.utils.Response;
+
+@Controller
+public class PaymentController {
+
+	@Value("${stripe.key.public}")
+	private String API_PUBLIC_KEY;
+
+	private StripeService stripeService;
+
+	public PaymentController(StripeService stripeService) {
+		this.stripeService = stripeService;
+	}
+
+	@GetMapping("/")
+	public String homepage() {
+		return "homepage";
+	}
+
+	@GetMapping("/charge")
+	public String chargePage(Model model) {
+		model.addAttribute("stripePublicKey", API_PUBLIC_KEY);
+		return "charge";
+	}
+
+	@PostMapping("/create-charge")
+	public @ResponseBody Response createCharge(String email, String token) {
+
+		String customer = stripeService.createCustomer(email,token);
+		if (token == null) {
+			return new Response(false, "Stripe payment token is missing. please try again later.");
+		}
+
+		String chargeId = stripeService.createCharge(email, token, 999,customer);// 9.99 usd
+
+		if (chargeId == null) {
+			return new Response(false, "An error accurred while trying to charge.");
+		}
+
+		// You may want to store charge id along with order information
+
+		return new Response(true, "Success your charge id is " + chargeId);
+	}
+}
